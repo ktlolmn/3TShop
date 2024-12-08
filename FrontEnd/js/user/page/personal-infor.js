90bb5bbe3ebc9dc32408177d95bef6ff8fc04383
thanhtruong7103 <=>
2024-12-09 01:06:16 +0700

    fix: ui bugs

--- FrontEnd/js/user/page/personal-infor.js
+++ FrontEnd/js/user/page/personal-infor.js
@@ -142,6 +142,7 @@ async function editInformationUser(data) {
             submitBtnInforForm.disabled = true
         }
     } catch (error) {
+        console.log('Lỗi nè: ',error)
         if(error.status === 408){
             console.log(errorPhone)
             phone.nextElementSibling.style.opacity = 1
